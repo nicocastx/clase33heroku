5033f54a8ec32df4f6a7e0b2314dc7159e04b5e5
nicocastx <nicocastx@gmail.com>
2023-03-03 14:47:21 -0300

    deploy en railways

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/mensaje', (req,res) => {
     res.send('Hola Node.js desde Railways!');
 })
 
+//por default, las nubes utilizan su propio puerto, debo darle siempre a la nube
+//la opcion de usar su propio puerto 
 const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor express escuchando en el puerto ${PORT}`)
