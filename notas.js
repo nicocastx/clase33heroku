b16c9ff3306cbb41c9330531ca70110562cb6b01
nicocastx <nicocastx@gmail.com>
2023-03-03 14:47:12 -0300

    first commit

---
+++ notas.js
@@ -0,0 +1,63 @@
+/**
+ * Aplicar y explorar el control de versiones
+ * SISTEMAS DE CONTROL DE VERSIONES DISTRIBUIDOS
+ * Cada ddesarrollador tenga una copia local del proyecto
+ * se crea una red distribuida de repositorios, en la que los desarrolladores utilizan un
+ * mecanismo de resolucion de conflictos 
+ * Un sistema de control de versiones distribuido nos permite:
+ *  Volver a cualquier estado anterior del proyecto
+ *  Tener una historia de cuales fueron los cambios
+ *  Saber como, cuando y quien realizo los cambios
+ *  Permite la colaboracion de un poryecto
+ *  Permite desarrollar versiones de un mismo proyecto al mismo tiempo
+ * dato: hash es una funcion matematica que traduce el codigo a otro
+ * 
+ * gitk - comando graciso a probar
+ * permite probar como una interfaz de arboles de git
+ *     
+ * PAAS
+ * Paas (platform as a service) es un entorno de desarrollo e implementacion completo en al nube
+ * Tipos de servicios en la nube:
+ * IAS (infraestructre as service)
+ * PAAS
+ * SAAS
+ * Cuenta con recursos que permiten generar 'de todo': desde aplicaciones sencillas basadas
+ * en la nube hasta aplicaciones empresariales
+ * Se compran recursos a un proveedor de servicios en la nube, a los que accedemos a traves
+ * de internet, pero solo pagamos por usarlos
+ * PaaS incluye infraestructura(servidores, almacenamiento y redes). tanto como middleware,
+ * sistemas de desarrollo, servicios de inteligencia empresarial, sistemas de adm de BD, etc.
+ * 
+ * Escenarios donde usar Paas:
+ *  Framework de desarrollo: PaaS proporciona framework que los desarrolladores pueden ampliar
+ * para hacer aplicaciones basadas en al nube.Permite a los desarrolladores crear aplicaciones
+ * con componentes de software integrados
+ *  Analisis/inteligencia empresarial(BI): Las herramientas que se proporcionan en PaaS
+ * permiten a las empresas realizar analisis de datos, obetener cierta informacion y
+ * poder predecir resultados para previsiones de una empresa
+ *  Servicios adicionales: Los proveedores de PaaS pueden ofrecer otros servicios que
+ * mejoren las aplicaciones, como workflow, directios, seguridad y programacion.
+ * 
+ * VENTAJAS DE PaaS
+ * Reduce el tiempo de programacion: Las herramientas de desarrollo de PaaS pueden reducir
+ * el tiempo que se tarda en programar aplicaciones con componentes ya programados
+ * Agrega funcionalidad de desarrollo sin incorporar mas personal: Los componentes de 
+ * plataforma como servicios pueden aportar al equipo de desarrollo nuevas caracteristicas sin necesidad
+ * de contratar personal especializado
+ * Desarrollar para varias plataformas facilmente: Algunas proveedores de servicios ofrecen opciones de
+ * desarrollo para varias plataformas, agilizando el desarrollo de aplicaciones multiplataforma
+ * 
+ * (no vimos heroku xd)
+ * RAILWAYS
+ * Primero debo elegir que aplicacion voy a lanzar sobre railways
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+//!RETOMAR MINUTO 1:35:20
